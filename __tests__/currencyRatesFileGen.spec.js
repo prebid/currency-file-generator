8fb1bb9fdebd4355f9742a08749083fec0a3a0dd
Isaac Dettman <idettman@rubiconproject.com>
2018-02-26 08:49:51 -0800

    Added unit tests for untested functions

--- __tests__/currencyRatesFileGen.spec.js
+++ __tests__/currencyRatesFileGen.spec.js
@@ -1,56 +1,64 @@
 const {http} = require('../__mocks__/http.mock');
-const spec = require('../src/currencyRatesFileGen');
+const spec = require('../src/currencyRatesFileGen').spec;
 
 describe(`Service aws-node-currency-rates-file-gen: S3 mock for successful operations`, () => {
     beforeAll(() => {
-
-    });
-
-    afterAll(() => {
         http.reset();
     });
 
-    test('getCurrencyUrl', () => {
+    test('constructCurrencyUrl', () => {
+        expect(spec.constructCurrencyUrl('FROMCURRENCY', 'AUD,BRL,CAD,CHF')).toEqual(
+            'http://api.fixer.io/latest?base=FROMCURRENCY&symbols=AUD,BRL,CAD,CHF');
 
-        expect(spec.getCurrencyUrl('FROMCURRENCY', 'AUD,BRL,CAD,CHF')).toEqual(
-                'http://api.fixer.io/latest?base=FROMCURRENCY&symbols=AUD,BRL,CAD,CHF');
-
-        expect(spec.getCurrencyUrl(undefined, 'AUD,BRL,CAD,CHF')).toEqual(
+        expect(spec.constructCurrencyUrl(undefined, 'AUD,BRL,CAD,CHF')).toEqual(
             'http://api.fixer.io/latest?base=undefined&symbols=AUD,BRL,CAD,CHF');
 
-        expect(spec.getCurrencyUrl(undefined, undefined)).toEqual(
+        expect(spec.constructCurrencyUrl(undefined, undefined)).toEqual(
             'http://api.fixer.io/latest?base=undefined&symbols=undefined');
 
-        expect(spec.getCurrencyUrl()).toEqual('http://api.fixer.io/latest?base=undefined&symbols=undefined');
+        expect(spec.constructCurrencyUrl()).toEqual('http://api.fixer.io/latest?base=undefined&symbols=undefined');
 
-        expect(spec.getCurrencyUrl(23,'AU')).toEqual('http://api.fixer.io/latest?base=23&symbols=AU');
+        expect(spec.constructCurrencyUrl(23, 'AU')).toEqual('http://api.fixer.io/latest?base=23&symbols=AU');
 
-        expect(spec.getCurrencyUrl('','')).toEqual('http://api.fixer.io/latest?base=&symbols=');
+        expect(spec.constructCurrencyUrl('', '')).toEqual('http://api.fixer.io/latest?base=&symbols=');
     });
 
     test('createDocument', () => {
         const doc = spec.createDocument([{
             base: 1.5,
-            rates: [4,65,90]
+            rates: [4, 65, 90]
         }, {
             base: 30.2,
-            rates: [2,4,65,90,23]
+            rates: [2, 4, 65, 90, 23]
         }]);
 
         expect(doc['conversions']).toEqual({"1.5": [4, 65, 90], "30.2": [2, 4, 65, 90, 23]});
     });
 
-    test('createDocument', () => {
-        const doc = spec.createDocument([{
-            base: 1.5,
-            rates: [4,65,90]
-        }, {
-            base: 30.2,
-            rates: [2,4,65,90,23]
-        }]);
+    test('requestCurrencyFile', () => {
 
-        expect(doc['conversions']).toEqual({"1.5": [4, 65, 90], "30.2": [2, 4, 65, 90, 23]});
     });
 
+    test('uploadDocumentToS3', () => {
 
+    });
+
+    test('getExpiration', () => {
+        const expiration = spec.getExpiration(365);
+        console.log('expiration:', expiration);
+    });
+
+    test('createDocumentParams', () => {
+        const doc = {"doc": {"content": ""}};
+        const docAsString = JSON.stringify(doc);
+        const expires = new Date(new Date().getTime() + 365 * 1000);
+        const docParams = spec.createDocumentParams('test', 'file1', doc, expires);
+
+        expect(docParams).toEqual({
+            Bucket: 'test',
+            Key: 'file1',
+            Body: docAsString,
+            Expires: expires
+        });
+    });
 });
--- src/currencyRatesFileGen.js
+++ src/currencyRatesFileGen.js
@@ -1,6 +1,10 @@
-// prebidCurrencyRatesFileGenerator
-// loads currency rates from api.fixer.io and creates a JSON representation in S3
-// revision:
+/**
+ * Loads currency rates from api.fixer.io and creates a JSON representation in S3.
+ * @fileOverview prebidCurrencyRatesFileGenerator
+ * @author Rubicon Project
+ * @version 1.0.1
+ */
+
 const http = require('http');
 const aws = require('aws-sdk');
 
@@ -9,79 +13,156 @@ const supportedCurrencies = 'AUD,BRL,CAD,CHF,CNY,CZK,DKK,EUR,GBP,HKD,HUF,IDR,ILS
 
 const bucket = 'currency.prebid.org';
 const filename = 'latest.json';
-const debug = true;
 const expires = 24 * 3600 + 5; // when to expire for HTTP "Expires:" header (seconds)
 
+const currencyAlertsEmail = 'alerts@prebid.org';
+
+const debug = true;
+
+/**
+ * @typedef {Object} S3UploadParams
+ * @property {string} Bucket - Name of the bucket to which the PUT operation was initiated.
+ * @property {string} Key - Object key for which the PUT operation was initiated.
+ * @property {Buffer|Array|Blob|String|ReadableStream} Body - Object data
+ * @property {Date} Expires - The date and time at which the object is no longer cacheable.
+ */
+
+/**
+ * @typedef {Object} FileProps
+ * @property {string} filename
+ * @property {ManagedUpload.SendData|undefined} error
+ */
+
+/**
+ * The function AWS Lambda calls to start execution of your Lambda function.
+ * You identify the handler when you create your Lambda function: IE 'Handler':'currencyRatesFilesGen.handler'
+ * @param event - AWS Lambda uses this parameter to pass in event data to the handler.
+ * @param context - the context parameter contains functions to access runtime information
+ */
 exports.handler = function(event, context) {
+    /**
+     * @type {Array.<Object>}
+     */
     const results = [];
     let countCompleted = 0;
 
     for (let fromCurrency of fromCurrencies) {
-        const url = getCurrencyUrl(fromCurrency, supportedCurrencies);
-        log('requesting: ' + url);
+        /**
+         * @type {string}
+         */
+        const currencyUrl = constructCurrencyUrl(fromCurrency, supportedCurrencies);
 
-        requestCurrencyFile(url, body => {
-            results.push(JSON.parse(body));
+        /**
+         * This object is created internally and returned from http.request().
+         * @type {http.ClientRequest} - documented at https://nodejs.org/api/http.html#http_class_http_clientrequest
+         */
+        const currencyFileRequest = requestCurrencyFile(currencyUrl, (json) => {
+            results.push(json);
             countCompleted++;
 
             if (countCompleted === fromCurrencies.length) {
-                log('rates s3 upload to: ' + bucket + ' ' + filename);
-                uploadDocumentToS3(filename, bucket, createDocument(results), (e, data) =>  {
-                    if (e) {
-                        logError(e, e.stack);
-                        context.done(null, JSON.stringify({ filename: filename, error: data }));
-                    } else {
-                        log('rates pushed to s3: ' + bucket + ' ' + filename);
-                        context.done(null, JSON.stringify({ filename: filename }));
-                    }
-                });
+                const docParams = createDocumentParams(bucket, filename, createDocument(results));
+                uploadDocumentToS3(docParams, context);
             }
         });
     }
 };
 
 /**
+ * @param {string} bucket
+ * @param {string} filename
+ * @param {Object} documentObj
+ * @param {Date} expires - when to expire for HTTP "Expires:"
+ * @return {S3UploadParams}
+ */
+function createDocumentParams(bucket, filename, documentObj, expires) {
+    return {
+        Bucket: bucket,
+        Key: filename,
+        Body: JSON.stringify(documentObj),
+        Expires: expires
+    }
+}
+
+/**
+ * @param {number} expires - when to expire for HTTP "Expires:" header (seconds)
+ * @return {Date}
+ */
+function getExpiration(expires) {
+    return new Date(new Date().getTime() + expires * 1000);
+}
+
+/**
  * @param {string} fromCurrency
  * @param {string} supportedCurrencies
  * @returns {string}
  */
-function getCurrencyUrl(fromCurrency, supportedCurrencies) {
+function constructCurrencyUrl(fromCurrency, supportedCurrencies) {
     return 'http://api.fixer.io/latest?base=' + fromCurrency + '&symbols=' + supportedCurrencies;
 }
 
 /**
  * @param {string} url
  * @param {function} fileEndCallback
+ * @returns {http.ClientRequest}
  */
 function requestCurrencyFile(url, fileEndCallback) {
-    http.get(url, res => {
+    log('requesting: ' + url);
+
+    return http.get(url, (res /** @type {http.IncomingMessage} */) => {
         let body = '';
-        res.on('data', d => {
-            body += d;
+
+        // the 'data' event is emitted whenever the stream is relinquishing ownership of a chunk of data to the consumer
+        res.on('data', (chunk) => { body += chunk; });
+
+        // the 'error' event emits if the stream is unable to generate data due to internal failure or from an invalid chunk of data
+        res.on('error', (e) => {
+            logError('request error', e);
         });
+
+        // The 'end' event is emitted after all data has been output.
         res.on('end', () => {
-            fileEndCallback(body);
+            try {
+                const json = JSON.parse(body);
+                fileEndCallback(json);
+            }
+            catch (e) {
+                logError('error parsing response', e);
+            }
         });
     });
 }
 
 /**
- * @param {string} filename
- * @param {string} bucket
- * @param {Object} document
- * @param {function} completeCallback
+ * @param {S3UploadParams} params
+ * @param context
  */
-function uploadDocumentToS3(filename, bucket, document, completeCallback) {
+function uploadDocumentToS3(params, context) {
+    log('rates s3 upload to: ' + params.Bucket + ' ' + params.Key);
+
+    /**
+     * @type {S3}
+     */
     const s3 = new aws.S3();
 
-    const param = {
-        Bucket: bucket,
-        Key: filename,
-        Body: JSON.stringify(document),
-        Expires: new Date(new Date().getTime() + expires * 1000)
-    };
+    /**
+     * Uploads an arbitrarily sized buffer, blob, or stream
+     * See {@link https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#upload-property} for more information
+     */
+    s3.upload(params, (e, data) => {
+        /** @type {FileProps} */
+        let fileProps;
 
-    s3.upload(param, completeCallback);
+        if (e) {
+            logError(e.toString(), e.stack);
+            fileProps = { filename: params.Key, error: data };
+        } else {
+            log('rates pushed to s3: ' + params.Bucket + ' ' + params.Key);
+            fileProps = { filename: params.Key, error: data };
+        }
+
+        context.done(null, JSON.stringify(fileProps));
+    });
 }
 
 /**
@@ -89,7 +170,11 @@ function uploadDocumentToS3(filename, bucket, document, completeCallback) {
  * @returns {{dataAsOf: string, conversions: {}}}
  */
 function createDocument(results) {
+    /**
+     * @type {Object}
+     */
     const conversions = {};
+
     for (let cv = 0; cv < results.length; cv++) {
         conversions[results[cv].base] = results[cv].rates;
     }
@@ -100,17 +185,26 @@ function createDocument(results) {
     };
 }
 
+/**
+ * @param {string} line
+ */
 function log (line) {
     if (debug) console.log(line);
 }
 
+/**
+ * @param {string} line
+ * @param {string|Error|Object} error
+ */
 function logError (line, error) {
     console.error(line, error);
 }
 
-/* TEST:START */
-exports.getCurrencyUrl = getCurrencyUrl;
-exports.requestCurrencyFile = requestCurrencyFile;
-exports.uploadDocumentToS3 = uploadDocumentToS3;
-exports.createDocument = createDocument;
-/* TEST:END */
+exports.spec = {
+    constructCurrencyUrl,
+    requestCurrencyFile,
+    uploadDocumentToS3,
+    createDocumentParams,
+    getExpiration,
+    createDocument
+};
